057088908b102fcdf781385e2bc2746b5e02d3f7
belvvvv <belvvvv@gmail.com>
2026-05-13 13:08:12 +0300

    Add files via upload

---
+++ ITOG_Sablin.py
@@ -0,0 +1,8 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python314/sushkov промежуточная.py
+
+= RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python314/Gorshkov_калькулятор.py
+>>> 
+= RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python314/ITOG_Sablin.py
